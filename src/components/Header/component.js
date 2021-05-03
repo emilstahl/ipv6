0d617a1559d990bea2e9dfce142c007a56a28af1
Alexander Matzen <alexander@amatzen.dk>
2021-05-03 22:19:22 +0200

    Header changes

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 import Logo from '../../images/logo.svg';
+import GitHubIcon from '../../images/icons/github.svg';
 
 const HeaderComponent = () => {
+    const [hover, setHover] = useState(false);
+
     return (
         <div className="header">
-            <div className="container">
+            <div className="container" style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
                 <div className="logo">
                   <a href="/" title="IPv6-adresse.dk" alt="Link to Home" style={{
                     color: '#333',
@@ -23,7 +29,13 @@ const HeaderComponent = () => {
                   </a>
                 </div>
                 <div className="contribute">
-                  <a href="" title="Hjælp os med at forbedre siden!"></a>
+                  <a href="https://www.github.com/emilstahl/ipv6" title="Hjælp os med at forbedre siden!">
+                    <GitHubIcon
+                      onMouseEnter={()=>setHover(true)}
+                      onMouseLeave={()=>setHover(false)}
+                      style={hover ? {opacity: 1, verticalAlign: 'middle'} : {opacity: 0.75, verticalAlign: 'middle'}}
+                    />
+                  </a>
                 </div>
             </div>
         </div>
